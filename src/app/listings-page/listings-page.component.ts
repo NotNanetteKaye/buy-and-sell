11da95a67a3addb274a3c4db1de4faffef2c222e
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-04-12 13:37:54 -0500

    What is selector for in Angular?

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
+/* selecter is what you insert to another component's HTML */
 @Component({
-  selector: 'app-listings-page',
+  selector: 'app-listings-page', 
   templateUrl: './listings-page.component.html',
   styleUrls: ['./listings-page.component.css']
 })
