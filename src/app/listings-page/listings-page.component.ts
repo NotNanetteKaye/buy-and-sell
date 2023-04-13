4072e7fbfff3374f9937f3436b90ac4dfc762c22
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-04-13 09:30:45 -0500

    ano ang structural directive?

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 /* selecter is what you insert to another component's HTML */
 @Component({
@@ -6,6 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './listings-page.component.html',
   styleUrls: ['./listings-page.component.css']
 })
-export class ListingsPageComponent {
+export class ListingsPageComponent implements OnInit {
+  listings: Listing[] = [];
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.listings = fakeListings;
+    
+  }
 }
