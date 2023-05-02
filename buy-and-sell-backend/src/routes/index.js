91a3f87ee91073578bb55d30441761183bf9b565
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-05-02 15:20:01 -0500

    UPDATE listings endpoint complete

--- buy-and-sell-backend/src/routes/index.js
+++ buy-and-sell-backend/src/routes/index.js
@@ -3,10 +3,12 @@ import { createNewListingRoute } from './createNewListing';
 import { getAllListingsRoute } from './getAllListings';
 import { getListingRoute } from './getListing';
 import { getUserListingsRoute } from './getUserListing';
+import { updateListingRoute } from './updateListing';
 
 export default [
     addViewToListingRoute,
     createNewListingRoute,
+    updateListingRoute,
     getAllListingsRoute,
     getListingRoute,
     getUserListingsRoute,
